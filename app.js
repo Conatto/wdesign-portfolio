0b5cb744c022cc5c97126b980b4a86f444c6e0ad
Conatto <leoconatto@gmail.com>
2023-05-14 18:04:09 -0300

    edit app file 3

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/"));
+
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/index.html");
+});
